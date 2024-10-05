aa0478ce3e291d170e59f2c074f77fa1d9be9aad
Mokhaled2004 <mokhaled2004@gmail.com>
2024-10-05 15:00:22 +0300

    DataBase Config file added for setting up the mysql database for the app | A helper methof that handles the uploads file is also added in the helpers.py file

---
+++ helpers.py
@@ -0,0 +1,9 @@
+
+# helpers.py
+import os
+
+# Allowed file extensions
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg', 'gif'}
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
