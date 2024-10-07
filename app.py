f73e7902124791d5b1762de5ba4b5c90a70fe250
Mokhaled2004 <mokhaled2004@gmail.com>
2024-10-07 16:00:14 +0300

    First Part of the profile Html page is Doneee

--- app.py
+++ app.py
@@ -96,5 +96,9 @@ def recipe():
 def tools():
     return render_template('tools.html')
 
+@app.route('/profile')
+def profile():
+    return render_template('profile.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
