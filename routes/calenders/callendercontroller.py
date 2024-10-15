cb955daf2c7cf0804e2764bf59abe20dd6d81543
Mokhaled2004 <mokhaled2004@gmail.com>
2024-10-15 23:13:22 +0300

    plans renderd in the calender

--- routes/calenders/callendercontroller.py
+++ routes/calenders/callendercontroller.py
@@ -6,6 +6,47 @@ calender_bp = Blueprint('calendercontroller', __name__)
 
 
 
+from flask import render_template, session, current_app
+from datetime import datetime
 @calender_bp.route('/calender')
 def calender():
-    return render_template('calender.html')
+    # Get the MySQL connection from Flask's current_app config
+    mysql = current_app.config['mysql']  
+    cursor = mysql.connection.cursor()
+
+    # Get the logged-in user's ID from the session
+    user_id = session.get('user_id')
+
+    # Get the current day of the week (0 = Monday, 6 = Sunday)
+    current_day = datetime.today().weekday()
+
+    # Convert to your DayNumber system (1 = Saturday, 7 = Friday)
+    day_number = (current_day + 3) % 7 or 7
+
+    # SQL query to join Trainer_User_Assignment and WorkoutDay tables
+    query = """
+        SELECT W.DayID, W.WorkoutID, W.DayNumber , W.name 
+        FROM Trainer_User_Assignment TUA
+        JOIN WorkoutDay W ON TUA.workoutid = W.WorkoutID
+        WHERE TUA.userid = %s AND W.DayNumber = %s
+    """
+
+    # Execute the query with the user_id and current day_number
+    cursor.execute(query, (user_id, day_number))
+    plans = cursor.fetchall()  # Fetch all matching plans
+
+    cursor.close()  # Close the cursor
+    
+    print(plans)
+
+    # If there are no plans for today
+    if not plans:
+        return render_template('calender.html', plans=None)
+
+    # Prepare the plan data to pass to the template
+    
+    
+
+    # Render the calender.html with the plans data
+    return render_template('calender.html', plans=plans)
+
